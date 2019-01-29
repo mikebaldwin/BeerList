56e2e7bc58e5b8a7436e7500fca926b8d3ba95f5
Michael Baldwin <baldwin@BaldwinMacbookPro.local>
2019-01-29 08:14:51 -0800

    Display beers in beer list view

--- Source/BeerList/BeerListViewController.swift
+++ Source/BeerList/BeerListViewController.swift
@@ -8,83 +8,62 @@
 
 import UIKit
 
+// MARK: - Named Constants
+private extension Int {
+    static var tableSections = 1
+}
+
+private extension String {
+    static var cellId = "BeerCell"
+}
+
+// MARK: - Class definition
+
 class BeerListViewController: UITableViewController {
 
+    let punkApi = PunkApi()
+    var beers: [Beer] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        title = "Beers"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: .cellId)
+        
+        punkApi.getBeerList(
+            success: { [weak self] beers in
+                self?.beers = beers
+                self?.tableView.reloadData()
+            },
+            failure: { error in
+                debugPrint("Error retrieving beers: \(error)")
+            }
+        )
     }
 
-    // MARK: - Table view data source
+}
+
+// MARK: - Table view data source
+extension BeerListViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return .tableSections
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return beers.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: .cellId, for: indexPath)
+        let beer = beers[indexPath.row]
+        cell.textLabel?.text = beer.name
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
+}
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+// MARK: - Table view delegate
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension BeerListViewController {
+    
 }
--- Source/Networking/PunkApi.swift
+++ Source/Networking/PunkApi.swift
@@ -6,9 +6,25 @@
 //  Copyright © 2019 mikebaldwin.co. All rights reserved.
 //
 
-import Alamofire
 import Foundation
+import Alamofire
 
 struct PunkApi {
     
+    func getBeerList(success: @escaping ([Beer]) -> Void,
+                     failure: @escaping (Error) -> Void) {
+        let endpoint = "https://api.punkapi.com/v2/beers"
+        Alamofire
+            .request(endpoint)
+            .validate()
+            .responseData(completionHandler: { response in
+                do {
+                    guard let data = response.result.value else { return }
+                    let beers = try JSONDecoder().decode([Beer].self, from: data)
+                    success(beers)
+                } catch {
+                    failure(error)
+                }
+            })
+    }
 }
